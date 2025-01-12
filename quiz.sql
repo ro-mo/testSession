64397ec41a92ebaf184e1bea6626540e111959f4
edofnj <edoardo@fmenegazzi.it>
2025-01-12 18:13:11 +0100

    change db

--- quiz.sql
+++ quiz.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Creato il: Gen 04, 2025 alle 21:17
+-- Creato il: Gen 12, 2025 alle 18:12
 -- Versione del server: 10.4.32-MariaDB
 -- Versione PHP: 8.2.12
 
@@ -34,79 +34,6 @@ CREATE TABLE `domanda` (
   `tipo` enum('multipla','libera') NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- --------------------------------------------------------
-
---
--- Struttura della tabella `risposta`
---
-
-CREATE TABLE `risposta` (
-  `id` int(11) NOT NULL,
-  `domanda_id` int(11) NOT NULL,
-  `testo` text NOT NULL,
-  `corretta` tinyint(1) DEFAULT 0
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- --------------------------------------------------------
-
---
--- Struttura della tabella `risposta_utente`
---
-
-CREATE TABLE `risposta_utente` (
-  `id` int(11) NOT NULL,
-  `utente_id` int(11) NOT NULL,
-  `domanda_id` int(11) NOT NULL,
-  `risposta_id` int(11) DEFAULT NULL,
-  `testo_libero` text DEFAULT NULL,
-  `data_risposta` timestamp NOT NULL DEFAULT current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- --------------------------------------------------------
-
---
--- Struttura della tabella `svolgimento_test`
---
-
-CREATE TABLE `svolgimento_test` (
-  `id` int(11) NOT NULL,
-  `test_id` int(11) NOT NULL,
-  `utente_id` int(11) NOT NULL,
-  `data_svolgimento` timestamp NOT NULL DEFAULT current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- --------------------------------------------------------
-
---
--- Struttura della tabella `test`
---
-
-CREATE TABLE `test` (
-  `id` int(11) NOT NULL,
-  `titolo` varchar(255) NOT NULL,
-  `descrizione` text DEFAULT NULL,
-  `creatore` varchar(100) NOT NULL,
-  `data_creazione` timestamp NOT NULL DEFAULT current_timestamp(),
-  `classe` varchar(100) DEFAULT NULL,
-  `visibile` tinyint(1) DEFAULT 0
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- --------------------------------------------------------
-
---
--- Struttura della tabella `utente`
---
-
-CREATE TABLE `utente` (
-  `id` int(11) NOT NULL,
-  `nome` varchar(100) NOT NULL,
-  `cognome` varchar(100) NOT NULL,
-  `login` varchar(100) NOT NULL,
-  `password` varchar(100) NOT NULL DEFAULT 'password',
-  `tipo` enum('docente','studente') NOT NULL DEFAULT 'studente',
-  `classe` varchar(100) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
 --
 -- Indici per le tabelle scaricate
 --
@@ -119,44 +46,6 @@ ALTER TABLE `domanda`
   ADD KEY `test_id` (`test_id`);
 
 --
--- Indici per le tabelle `risposta`
---
-ALTER TABLE `risposta`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `domanda_id` (`domanda_id`);
-
---
--- Indici per le tabelle `risposta_utente`
---
-ALTER TABLE `risposta_utente`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `utente_id` (`utente_id`),
-  ADD KEY `domanda_id` (`domanda_id`),
-  ADD KEY `risposta_id` (`risposta_id`);
-
---
--- Indici per le tabelle `svolgimento_test`
---
-ALTER TABLE `svolgimento_test`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `test_id` (`test_id`),
-  ADD KEY `utente_id` (`utente_id`);
-
---
--- Indici per le tabelle `test`
---
-ALTER TABLE `test`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `creatore` (`creatore`);
-
---
--- Indici per le tabelle `utente`
---
-ALTER TABLE `utente`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `index_login_utente` (`login`);
-
---
 -- AUTO_INCREMENT per le tabelle scaricate
 --
 
@@ -164,37 +53,7 @@ ALTER TABLE `utente`
 -- AUTO_INCREMENT per la tabella `domanda`
 --
 ALTER TABLE `domanda`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
-
---
--- AUTO_INCREMENT per la tabella `risposta`
---
-ALTER TABLE `risposta`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
-
---
--- AUTO_INCREMENT per la tabella `risposta_utente`
---
-ALTER TABLE `risposta_utente`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
-
---
--- AUTO_INCREMENT per la tabella `svolgimento_test`
---
-ALTER TABLE `svolgimento_test`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT per la tabella `test`
---
-ALTER TABLE `test`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
-
---
--- AUTO_INCREMENT per la tabella `utente`
---
-ALTER TABLE `utente`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
 -- Limiti per le tabelle scaricate
@@ -205,33 +64,6 @@ ALTER TABLE `utente`
 --
 ALTER TABLE `domanda`
   ADD CONSTRAINT `domanda_ibfk_1` FOREIGN KEY (`test_id`) REFERENCES `test` (`id`) ON DELETE CASCADE;
-
---
--- Limiti per la tabella `risposta`
---
-ALTER TABLE `risposta`
-  ADD CONSTRAINT `risposta_ibfk_1` FOREIGN KEY (`domanda_id`) REFERENCES `domanda` (`id`) ON DELETE CASCADE;
-
---
--- Limiti per la tabella `risposta_utente`
---
-ALTER TABLE `risposta_utente`
-  ADD CONSTRAINT `risposta_utente_ibfk_1` FOREIGN KEY (`utente_id`) REFERENCES `utente` (`id`),
-  ADD CONSTRAINT `risposta_utente_ibfk_2` FOREIGN KEY (`domanda_id`) REFERENCES `domanda` (`id`),
-  ADD CONSTRAINT `risposta_utente_ibfk_3` FOREIGN KEY (`risposta_id`) REFERENCES `risposta` (`id`);
-
---
--- Limiti per la tabella `svolgimento_test`
---
-ALTER TABLE `svolgimento_test`
-  ADD CONSTRAINT `svolgimento_test_ibfk_1` FOREIGN KEY (`test_id`) REFERENCES `test` (`id`) ON DELETE CASCADE,
-  ADD CONSTRAINT `svolgimento_test_ibfk_2` FOREIGN KEY (`utente_id`) REFERENCES `utente` (`id`) ON DELETE CASCADE;
-
---
--- Limiti per la tabella `test`
---
-ALTER TABLE `test`
-  ADD CONSTRAINT `test_ibfk_1` FOREIGN KEY (`creatore`) REFERENCES `utente` (`login`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
